9a08fe26c89b6fc05299d62df78b66ed97afa244
Lilian Cardenas <lili.denisse2@gmail.com>
2023-07-21 12:15:51 -0400

    Arreglamos el login para que quede mas grande

--- src/Components/form.jsx
+++ src/Components/form.jsx
@@ -3,11 +3,11 @@ import * as React from 'react';
 
 export default function Form(){
 return (
-    <div className='bg-white px-10 py-20 rounded-3xl border-2'>
+    <div className='bg-white px-10 py-20 rounded-3xl border-2 w-full mx-10'>
         <h1 className='text-5xl font-lobster'>Logo Burger queen</h1> 
-        <div className='mt-8'>
+        <div className='mt-10'>
             <div>
-                <label className='text-lg font-medium'>Ingrese usuario</label>
+                <label className='text-2xl font-large'>Ingrese usuario</label>
                 <input
                     className='focus:outline-none focus:border-rose-500 w-full border-2 border-amber-500 rounded-lg p-3 mt-1'
                     placeholder='nombre@xxx.xxx'
@@ -15,8 +15,8 @@ return (
                 />
             </div>
 
-            <div className='mt-4'>
-                <label className='text-lg font-medium'>Ingrese contraseña</label>
+            <div className='mt-7'>
+                <label className='text-2xl font-large'>Ingrese contraseña</label>
                 <input
                     className='focus:outline-none focus:border-rose-500 w-full border-2 border-amber-500 rounded-lg p-3 mt-1'
                     placeholder='contraseña'
@@ -24,12 +24,12 @@ return (
                 />
             </div>
 
-            <div className='mt-8 flex justify-center item-center '>
+            <div className='mt-10 flex justify-center item-center '>
                 <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out text-3xl bg-cyan-700 text-white w-full p-3 rounded-lg font-lobster'>Iniciar sesión</button>
             </div>
 
-            <div className='mx-auto w-3/6 items-center '> 
-                <p className='text-sm mt-4 flex text-center items-center justify-center font-judson'>Si no estas registrado, avisar a administración</p>
+            <div className='mx-auto w-3/6 items-center'> 
+                <p className='text-xl mt-4 flex text-center items-center justify-center font-judson'>Si no estas registrado, avisar a administración</p>
             </div>
         </div>
     </div>
