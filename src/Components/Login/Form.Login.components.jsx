892d51c7839c398f80e96252965cb12f260964a1
Jae <one.sweet.dreams@gmail.com>
2023-07-23 23:33:01 -0400

    Se arreglan estilo y se termina creacion de vista wellcome

--- src/Components/Login/Form.Login.components.jsx
+++ src/Components/Login/Form.Login.components.jsx
@@ -4,17 +4,27 @@ import * as React from 'react';
 function Form() {
     return (
         <>
-            <section className='bg-white px-10 pb-20 rounded-3xl border-2 w-full mx-10'>
+            <section className='bg-white px-10 pb-20 rounded-3xl border-2 w-full mx-10 drop-shadow-md'>
                 <div>
-                    <img src="/3.png" alt="Logo Burger Queen" className='block lg:hidden h-2/4 mx-auto' />
-                    <img src="/Burger-Queen.png" alt="Burger Queen" className='hidden lg:block' />
+                    <img 
+                    src="/3.png" 
+                    alt="Logo Burger Queen" 
+                    className='block lg:hidden h-2/4 mx-auto' 
+                    />
+
+                    <img 
+                    src="/Burger-Queen.png" 
+                    alt="Burger Queen" 
+                    className='hidden lg:block' 
+                    />
+                    
                 </div>
 
                 <form className='mt-10'>
                     <div>
                         <label className='text-2xl font-large ml-5'>Ingrese usuario</label>
                         <input
-                            className='focus:outline-none focus:border-rose-500 w-full border-2 text-lg border-amber-500 rounded-full p-4 mt-1 h-20 font-judson'
+                            className='focus:outline-none focus:border-rose-500 w-full border-2 text-lg border-[#F5A25D] rounded-full p-4 mt-1 h-20 font-judson drop-shadow-md'
                             placeholder='Nombre@XXX.XXX'
                             type='text'
                         />
@@ -23,19 +33,20 @@ function Form() {
                     <div className='mt-7'>
                         <label className='text-2xl font-large ml-5'>Ingrese contraseña</label>
                         <input
-                            className='focus:outline-none focus:border-rose-500 w-full border-2 text-lg border-amber-500 rounded-full p-4 mt-1 h-20 font-judson'
+                            className='focus:outline-none focus:border-rose-500 w-full border-2 text-lg border-[#F5A25D] rounded-full p-4 mt-1 h-20 font-judson drop-shadow-md'
                             placeholder='Contraseña'
                             type='password'
                         />
                     </div>
 
                     <div className='mt-10 flex justify-center item-center'>
-                        <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out text-3xl bg-cyan-700 text-white w-full p-4 h-20 rounded-full font-lobster'>Iniciar sesión</button>
+                        <button 
+                        className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out text-5xl bg-cyan-700 text-white w-full p-4 h-20 rounded-full font-lobster drop-shadow-md '>Iniciar Sesión</button>
                     </div>
                 </form>
 
                 <div className='mx-auto w-3/6 items-center'>
-                    <p className='text-xl mt-4 flex text-center items-center justify-center font-judson'>Si no estas registrado, avisar a administración</p>
+                    <p className='text-lg mt-4 flex text-center items-center justify-center font-judson'>Si no estas registrado, avisar a administración</p>
                 </div>
             </section>
         </>
@@ -53,7 +64,13 @@ function Logo() {
                 </div>
 
                 <div className='hidden lg:flex w-1/2 h-full items-center justify-center bg-white'>
-                    <img src="/logo-solo-hamburguesa.png" alt="Logo Burger Queen" className='animate-bounce w-2/3' />
+
+                    <img 
+                    src="/logo-solo-hamburguesa.png" 
+                    alt="Logo Burger Queen" 
+                    className='animate-bounce w-3/4 ' 
+                    />
+                    
                 </div>
 
             </div>
--- src/Components/Wellcome/wellcomeApp.jsx
+++ src/Components/Wellcome/wellcomeApp.jsx
@@ -1,71 +1,102 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
-
-function BQueen() {
-// 1. Creamos un estado llamado 'isPortrait' que indica si el dispositivo está en orientación vertical (true) o horizontal (false).
-    const [isPortrait, setIsPortrait] = useState(true);
-
-// 2. Utilizamos useEffect para ejecutar el código cuando el componente se monta y cada vez que cambia la orientación del dispositivo.
-    useEffect(() => {
-// 3. Definimos una función 'handleOrientationChange' que actualiza el estado 'isPortrait' en función de la orientación actual del dispositivo.
-        const handleOrientationChange = () => {
-            setIsPortrait(window.matchMedia('(orientation: portrait)').matches);
-        };
-// 4. Ejecutamos 'handleOrientationChange' cuando el componente se monta inicialmente para establecer el valor inicial de 'isPortrait'.
-        handleOrientationChange();
-
-// 5. Agregamos un event listener para el evento 'orientationchange', que se disparará cada vez que el dispositivo cambie de orientación.
-        window.addEventListener('orientationchange', handleOrientationChange);
-
-// 6. Utilizamos el 'return' para limpiar el event listener cuando el componente se desmonte, evitando pérdida de memoria.
-        return () => {
-            window.removeEventListener('orientationchange', handleOrientationChange);
-        };
-    }, []);
-
-// 7. Calculamos la variable 'bgColor' que determina el color de fondo en función del estado 'isPortrait'.
-    const bgColor = isPortrait ? '#00ff00' : '#ff0000';
-
-    return (
-        <>
-         {/* 8. Creamos un contenedor div con la clase y estilos necesarios. El color de fondo se ajusta según el valor de 'bgColor'. */}
-            <div className={`bg-[url('fondo.jpg')] bg-no-repeat bg-center bg-cover min-h-screen flex items-center justify-center opacity-80`} style={{ backgroundColor: bgColor }}>
-                <div>
-                    <img src='3.png' alt='Logo BQ' className="" />
-                </div>
-            </div>
-        </>
-    );
-}
-
-export {BQueen};
-// import * as React from "react";
+// /* eslint-disable no-unused-vars */
+// import React, { useEffect, useState } from 'react'; // son Hook
+// //El código que escribas dentro de useEffect se ejecutará cada vez que el componente se monte, en este caso horizontal y vertical.
+// //Permite que los componentes funcionales se comporten de manera similar a los componentes de clase en cuanto a manejo de estado, permite actualizar el estado las veces necesarias
 
 // function BQueen() {
+// // 1. Creamos un estado llamado 'isPortrait' que indica si el dispositivo está en orientación vertical (true) o horizontal (false).
+//     const [isPortrait, setIsPortrait] = useState(true);
+
+// // 2. Utilizamos useEffect para ejecutar el código cuando el componente se monta y cada vez que cambia la orientación del dispositivo.
+//     useEffect(() => {
+// // 3. Definimos una función 'handleOrientationChange' que actualiza el estado 'isPortrait' en función de la orientación actual del dispositivo.
+//         const handleOrientationChange = () => {
+//             setIsPortrait(window.matchMedia('(orientation: portrait)').matches);
+//         };
+// // 4. Ejecutamos 'handleOrientationChange' cuando el componente se monta inicialmente para establecer el valor inicial de 'isPortrait'.
+//         handleOrientationChange();
+
+// // 5. Agregamos un event listener para el evento 'orientationchange', que se disparará cada vez que el dispositivo cambie de orientación.
+//         window.addEventListener('orientationchange', handleOrientationChange);
+
+// // 6. Utilizamos el 'return' para limpiar el event listener cuando el componente se desmonte, evitando pérdida de memoria.
+//         return () => {
+//             window.removeEventListener('orientationchange', handleOrientationChange);
+//         };
+//     }, []);
+
+// // 7. Calculamos la variable 'bgColor' que determina el color de fondo en función del estado 'isPortrait'.
+//     const bgColor = isPortrait ? '#00ff00' : '#ff0000';
+
 //     return (
 //         <>
-//             <div className="bg-[url('fondo.jpg')] bg-no-repeat bg-center bg-cover min-h-screen flex items-center justify-center opacity-80">
+//          {/* 8. Creamos un contenedor div con la clase y estilos necesarios. El color de fondo se ajusta según el valor de 'bgColor'. */}
+//             <div className={`bg-[url('fondo.jpg')] bg-no-repeat bg-center bg-cover min-h-screen flex items-center justify-center opacity-80`} style={{ backgroundColor: bgColor }}>
 //                 <div>
-//                     <img src='3.png' alt='Logo BQ' className=""/>
+//                     <img src='3.png' alt='Logo BQ' className="" />
 //                 </div>
 //             </div>
-//             <style>
-//                 {`
-//             @media (orientation: landscape) {
-//                 .bg-[url('fondo.jpg')] {
-//                 background-color: #ff0000; /* Color de fondo horizontal */
-//                 }
-//                 }
-
-//             @media (orientation: portrait) {
-//                 .bg-[url('fondo.jpg')] {
-//                 background-color: #00ff00; /* Color de fondo vertical */
-//                 }
-//                 }
-//                 `}
-//             </style>
 //         </>
 //     );
 // }
 
-// export { BQueen };
+// export {BQueen};
+// import * as React from "react";
+
+import React from 'react';
+
+function BQueen() {
+    return (
+        <>
+
+            <style>
+                {`
+          /* Fondo vertical solo en orientación vertical */
+            @media (orientation: portrait) {
+            .bg-vertical {
+                background-image: url('fondo.jpg');
+            }
+            }
+
+          /* Fondo horizontal solo en orientación horizontal */
+            @media (orientation: landscape) {
+            .bg-horizontal {
+                background-image: url('fondoVertical.jpg');
+            }
+            }
+        `}
+            </style>
+
+            <section className="bg-no-repeat bg-center bg-cover min-h-screen flex items-center justify-center opacity-80 bg-vertical bg-horizontal m-0">
+                <div className="flex flex-col items-center ">
+                    <div>
+                        {/* Imagen con tamaño diferente en orientación horizontal */}
+                        <img
+                            src="3.png"
+                            alt="Logo BQ"
+                            className="w-5/6 mx-auto lg:w-3/5" // Ancho 5/6 en orientación vertical, ancho 1/4 en orientación horizontal
+                        />
+
+                        {/* Imagen con tamaño diferente en orientación horizontal */}
+                        <img
+                            src="Lili-Jae.png"
+                            alt="Octocat Lili y Jae"
+                            className="w-2/4 mx-auto lg:w-1/4 mt-4 drop-shadow-xl " // Ancho 2/4 en orientación vertical, ancho 1/4 en orientación horizontal
+                        />
+                    </div>
+
+                    <div className='flex justify-center items-center m-7'>
+                        <button
+                            type="button"
+                            className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out text-7xl drop-shadow-md shadow-lg w-[32rem] mt-8 bg-cyan-700 text-white h-20 rounded-full font-lobster text-center'>
+                            Entrar
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export { BQueen };
