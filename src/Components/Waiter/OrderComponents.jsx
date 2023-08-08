a8d2e083db58bdab2f4e0ab4a445a60c435d933c
Lilian Cardenas <lili.denisse2@gmail.com>
2023-08-08 14:43:34 -0400

    Intentando hacer responsive la parte del mesero

--- src/Components/Waiter/OrderComponents.jsx
+++ src/Components/Waiter/OrderComponents.jsx
@@ -10,7 +10,7 @@ import LogoBQueen from '/3.png';
 function ImgLogo() {
     return (
         <>
-            <div>
+            <div className='lg:mt-[-35px] w-[80%]'>
                 <img className='flex justify-center h-55 w-full '
                 src={LogoBQueen} alt="Logo burger queen" />
             </div>
@@ -20,7 +20,7 @@ function ImgLogo() {
 
 function InfoClient() {
 
-    const [workersName, setWorkersName] = useState('');
+    /*const [workersName, setWorkersName] = useState('');
     useEffect(() => {
         const loginOptions = {
             body: {
@@ -39,25 +39,25 @@ function InfoClient() {
         };
 
         fetchWorkersName();
-    }, []);
+    }, []);*/
     return (
         <>
 
-            <div className=' font-judson text-2xl border-b-2 border-b-[#F67575] w-[90%]'>
+            <div className=' font-judson text-2xl border-b-2 border-b-[#F67575] w-[90%] lg:w-[75%]'>
                 <label htmlFor="">Garzón:</label>
-                <p className='font-judson ml-2 mt-3'> {workersName || "S.N."} </p>
+                <p className='font-judson ml-2 mt-3'> </p>
             </div>
-
+                      {/* {workersName || "S.N."} */}
             <div>
                 <input
-                    className='focus:outline-none focus:border-rose-500 w-full border-2 text-2xl border-[#F5A25D] rounded-full p-4 mt-1 h-1/2 font-judson drop-shadow-md bg-[#FFE1CD] placeholder-gray-500'
+                    className='focus:outline-none focus:border-rose-500 w-full border-2 text-2xl border-[#F5A25D] rounded-full p-4 mt-1 h-1/2 font-judson drop-shadow-md bg-[#FFE1CD] placeholder-gray-500 lg:w-[75%]'
                     placeholder='Ingrese cliente'
                     type='text'
                 />
             </div>
             <div>
                 <input
-                    className='focus:outline-none focus:border-rose-500 w-full border-2 text-2xl border-[#F5A25D] rounded-full p-4 mt-1 h-1/2 font-judson drop-shadow-md bg-[#FFE1CD] placeholder-gray-500'
+                    className='focus:outline-none focus:border-rose-500 w-full border-2 text-2xl border-[#F5A25D] rounded-full p-4 mt-1 h-1/2 font-judson drop-shadow-md bg-[#FFE1CD] placeholder-gray-500 lg:w-[75%]'
                     placeholder='Ingrese n° Mesa'
                     type='text'
                 />
@@ -76,11 +76,11 @@ function NavPrincipal() {
 
     return (
         <>
-            <nav className='flex justify-center my-4 h-full'>
-                <ul className='flex'>
-                    <li className="mr-1">
+            <nav className='flex justify-center my-6'>
+                <ul className='lg:flex'>
+                    <li>
                         <a
-                            className={`bg-white inline-block rounded-t-lg py-2 px-4 text-gray-500 hover:text-black font-judson text-4xl ${selectedItem === 'desayuno' ? 'border-t-4 border-x-4 border-[#389393]' : 'border-b-4 border-b-[#389393]'
+                            className={`bg-white inline-block rounded-t-lg py-2 px-4 text-gray-500 hover:text-black font-judson text-4xl  ${selectedItem === 'desayuno' ? 'border-t-4 border-x-4 border-[#389393]' : 'border-b-4 border-b-[#389393]'
                                 }`}
                             href="#"
                             onClick={() => handleClick('desayuno')}>
@@ -102,6 +102,7 @@ function NavPrincipal() {
                             onClick={() => handleClick('pedido')}>
                             Pedido Listos
                         </a>
+                        
                     </li>
                 </ul>
             </nav>
@@ -119,8 +120,8 @@ function NavPrincipal() {
 
 function BtnSendOrder() {
     return (
-        <div className='fixed bottom-10 w-2/3'>
-            <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out text-5xl bg-cyan-700 text-white w-full p-4 h-20 rounded-full font-lobster shadow-xl '
+        <div className=' mt-32 w-2/3 justify-center'>
+            <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out text-5xl bg-cyan-700 text-white w-full p-4 h-20 rounded-full font-lobster shadow-xl lg:mb-20'
                 type='submit'>Enviar Pedido</button>
         </div>
     )
