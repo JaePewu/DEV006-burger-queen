c71ed0add42986a75f1d2720cea01c503842df63
Jae <one.sweet.dreams@gmail.com>
2023-08-08 20:46:37 -0400

    Se realiza funcion para cerrar sesion

--- src/Components/LogOut/LogOutComponents.jsx
+++ src/Components/LogOut/LogOutComponents.jsx
@@ -1,11 +1,26 @@
+import { useCallback } from 'react';// es un hook para no recargar la pagina
+import { Link } from 'react-router-dom'; //Link para redireccionar a otra página
+
 import CerrarSesion from '/CerrarSesion.png'
 
 function LogOut() {
-    return(
+    
+
+    const handleLogOut = useCallback(() => {
+        //useCallback se usa para no recargar la pagina y que se ejecute solo una vez, no se ejecute cada vez que se cambia el estado
+        localStorage.removeItem('token');
+        localStorage.removeItem('workers');
+        console.log('Sesión cerrada');
+    }, []);
+    console.log(handleLogOut + 'Se Presiono el LogOut');
+
+    return (
         <>
-        <img 
-        className='flex  h-20' 
-        src= {CerrarSesion} alt="" />
+            <Link to="/login" onClick={handleLogOut}>
+                <img
+                    className='flex  h-20 transition-all hover:scale-110'
+                    src={CerrarSesion} alt="" />
+            </Link>
         </>
     )
 }
